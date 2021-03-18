4b32e3f997f94b4703f8e68aa38dad24e7eb7cfd
Ertan Turan <ertanturan@outlook.com>
2021-03-18 20:25:17 +0300

    summaries updated

--- GeodesyLib/Calculations.cs
+++ GeodesyLib/Calculations.cs
@@ -8,9 +8,8 @@ namespace GeodesyLib
         // This uses the ‘haversine’ formula to calculate the great-circle distance between two points – that is,
         // the shortest distance over the earth’s surface – giving an ‘as-the-crow-flies’ distance between the points
         // (ignoring any hills they fly over, of course!).
-        public static double HaversineDistance(Coordinate from , Coordinate to  )
+        public static double HaversineDistance(Coordinate from, Coordinate to)
         {
-            
             double lat1 = Utility.ConvertToRadian(to.Lat);
             double lat2 = Utility.ConvertToRadian(from.Lat);
 
@@ -20,9 +19,9 @@ namespace GeodesyLib
             double latDelta = lat2 - lat1;
             double lonDelta = lon2 - lon1;
 
-            double a = Math.Pow(Math.Sin(latDelta / 2),2)  +
-                       Math.Cos(lat1) * Math.Cos(lat2) * 
-                       Math.Pow( Math.Sin(lonDelta / 2),2);
+            double a = Math.Pow(Math.Sin(latDelta / 2), 2) +
+                       Math.Cos(lat1) * Math.Cos(lat2) *
+                       Math.Pow(Math.Sin(lonDelta / 2), 2);
 
             double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
@@ -31,15 +30,18 @@ namespace GeodesyLib
             return d;
         }
 
-        // This makes the simpler law of cosines a reasonable 1-line alternative to the haversine formula
-        // for many geodesy purposes (if not for astronomy). 
-        // The choice may be driven by programming language, processor, coding context, available trig
-        // functions (in different languages), etc – and, for very small distances an equirectangular approximation
-        // may be more suitable.
-
+        /// <summary>
+        ///    This makes the simpler law of cosines a reasonable 1-line alternative to the haversine formula
+        /// for many geodesy purposes (if not for astronomy). 
+        /// The choice may be driven by programming language, processor, coding context, available trig
+        /// functions (in different languages), etc – and, for very small distances an equirectangular approximation
+        /// may be more suitable.
+        /// </summary>
+        /// <param name="from">first coordinate, starting point of a destination </param>
+        /// <param name="to">second coordinate, end point of a destination</param>
+        /// <returns></returns>
         public static double SphericalLawOfCosines(Coordinate from, Coordinate to)
         {
-
             double lat1 = Utility.ConvertToRadian(from.Lat);
             double lat2 = Utility.ConvertToRadian(to.Lat);
 
@@ -54,13 +56,19 @@ namespace GeodesyLib
             return 0;
         }
 
-        // This formula is for the initial bearing (sometimes referred to as forward azimuth) which if followed
-        // in a straight line along a great-circle arc will take you from the start point to the end point
+        /// <summary>
+        ///  This formula is for the initial bearing (sometimes referred to as forward azimuth) which if followed
+        /// in a straight line along a great-circle arc will take you from the start point to the end point
+        /// 
+        /// </summary>
+        /// <param name="from">first coordinate, starting point of a destination </param>
+        /// <param name="to">second coordinate, end point of a destination</param>
+        /// <returns></returns>
         public static double CalculateInitialBearing(Coordinate from, Coordinate to)
         {
             double lat1 = Utility.ConvertToRadian(from.Lat);
             double lat2 = Utility.ConvertToRadian(to.Lat);
-            
+
             double lon1 = Utility.ConvertToRadian(from.Lon);
             double lon2 = Utility.ConvertToRadian(to.Lon);
 
@@ -74,45 +82,46 @@ namespace GeodesyLib
             double bearing = (teta * 180 / Constants.PI + 360) % 360;
 
             return bearing;
-
-
         }
 
         public static Coordinate CalculateMidPoint(Coordinate from, Coordinate to)
         {
-
             double lat1 = Utility.ConvertToRadian(from.Lat);
             double lat2 = Utility.ConvertToRadian(to.Lat);
-            
+
             double lon1 = Utility.ConvertToRadian(from.Lon);
             double lon2 = Utility.ConvertToRadian(to.Lon);
-            
-            double lonDelta = lon2 -lon1;
+
+            double lonDelta = lon2 - lon1;
 
             double bX = Math.Cos(lat2) * Math.Cos(lonDelta);
             double bY = Math.Cos(lat2) * Math.Sin(lonDelta);
 
             double cosBx = Math.Cos(lat1) + bX;
-            
+
             double cosBxPow = Math.Pow(cosBx, 2);
             double bY2 = Math.Pow(bY, 2);
 
 
             double newLat = Math.Atan2(
-                Math.Sin(lat1) + Math.Sin(lat2), 
-                Math.Sqrt(cosBxPow+ bY2)
+                Math.Sin(lat1) + Math.Sin(lat2),
+                Math.Sqrt(cosBxPow + bY2)
             );
-                
-                
+
 
             double newLon = lon1 + Math.Atan2(bY, Math.Cos(lat1) + bX);
             newLon = (newLon + 540) % 360 - 180;
-            
+
             return new Coordinate(
                 Utility.ConvertToDegree(newLat),
                 Utility.ConvertToDegree(newLon));
         }
-        
-        
+
+        /// <summary>
+        /// An intermediate point at any fraction along the great circle path between two points 
+        /// </summary>
+        public static void CalculateMidPoint()
+        {
+        }
     }
 }
