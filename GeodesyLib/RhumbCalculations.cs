8d5dfa39275f9399ed2d5238a9a1eff9cc885778
Ertan Turan <ertanturan@outlook.com>
2021-06-13 17:41:14 +0300

    rhumb midpoint calculation added

--- GeodesyLib/RhumbCalculations.cs
+++ GeodesyLib/RhumbCalculations.cs
@@ -16,11 +16,11 @@ namespace GeodesyLib
         /// <returns>Returns the distance between two points along a rhumb line</returns>
         public static double CalculateRhumbDistance(this Coordinate from, Coordinate to)
         {
-            double lat1 = from.Lat.ConvertToRadian();
-            double lon1 = from.Lon.ConvertToRadian();
+            double lat1 = from.Lat.ConvertDegreeToRadian();
+            double lon1 = from.Lon.ConvertDegreeToRadian();
 
-            double lat2 = to.Lat.ConvertToRadian();
-            double lon2 = to.Lon.ConvertToRadian();
+            double lat2 = to.Lat.ConvertDegreeToRadian();
+            double lon2 = to.Lon.ConvertDegreeToRadian();
 
             double delta = Math.Log(
                 Math.Tan(
@@ -46,13 +46,13 @@ namespace GeodesyLib
             return dist;
         }
 
-        public static double CalculateRhumbInitialBearing(this Coordinate from, Coordinate to)
+        public static double CalculateRhumbBearing(this Coordinate from, Coordinate to)
         {
-            double lat1 = from.Lat.ConvertToRadian();
-            double lon1 = from.Lon.ConvertToRadian();
+            double lat1 = from.Lat.ConvertDegreeToRadian();
+            double lon1 = from.Lon.ConvertDegreeToRadian();
 
-            double lat2 = to.Lat.ConvertToRadian();
-            double lon2 = to.Lon.ConvertToRadian();
+            double lat2 = to.Lat.ConvertDegreeToRadian();
+            double lon2 = to.Lon.ConvertDegreeToRadian();
 
             double lonDelta = lon2 - lon1;
 
@@ -76,9 +76,9 @@ namespace GeodesyLib
             double bearing)
         {
 
-            double rBearing = bearing.ConvertToRadian();
-            double lat1 = from.Lat.ConvertToRadian();
-            double lon1 = from.Lon.ConvertToRadian();
+            double rBearing = bearing.ConvertDegreeToRadian();
+            double lat1 = from.Lat.ConvertDegreeToRadian();
+            double lon1 = from.Lon.ConvertDegreeToRadian();
 
             double delta = distance / Constants.RADIUS;
             double deltaLat = delta * Math.Cos(rBearing);
@@ -88,15 +88,47 @@ namespace GeodesyLib
             double deltaResult = Math.Log(
                 Math.Tan(lat2 / 2 + Constants.PI / 4) / Math.Tan(lat1 / 2 + Constants.PI / 4)
             );
-
-            double q = Math.Abs(deltaResult) > 10e-12 ? deltaLat / deltaResult : Math.Cos(lat1); // E-W course becomes ill-conditioned with 0/0
+            
+            // E-W course becomes ill-conditioned with 0/0
+            double q = Math.Abs(deltaResult) > 10e-12 ? deltaLat / deltaResult : Math.Cos(lat1);
+            
             double deltaLon = delta * Math.Sin(rBearing) / q;
             double lon2 = lon1 + deltaLon;
 
             return new Coordinate(
-                lat2.ConvertToDegree(),
-                lon2.ConvertToDegree()
+                lat2.ConvertRadianToDegree(),
+                lon2.ConvertRadianToDegree()
                 );
+        }
+
+
+        public static Coordinate CalculateRhumbMidPoint(this Coordinate from, Coordinate to)
+        {
+            double lat1 = from.Lat.ConvertDegreeToRadian();
+            double lon1 = from.Lon.ConvertDegreeToRadian();
+
+            double lat2 = to.Lat.ConvertDegreeToRadian();
+            double lon2 = to.Lon.ConvertDegreeToRadian();
+
+            double deltaLon = lon2 - lon1;
+            
+            double latMid = (lat1 + lat2) / 2;
+            double quarterPi = Constants.PI * 4;
+            
+            double f1 = Math.Tan(quarterPi + lat1 / 2);
+            double f2 = Math.Tan(quarterPi + lat2 / 2);
+            double f3 = Math.Tan(quarterPi + latMid / 2);
+
+            double lonMid = (deltaLon * Math.Log(f3) + lon1 * Math.Log(f2) - lon2 * Math.Log(f1))
+                            / Math.Log(f2 / f1);
+            //longitude normalisation
+            lonMid = (lonMid + 540) % 360 - 180;
+
+            return new Coordinate(latMid.ConvertRadianToDegree(),
+                lonMid.ConvertRadianToDegree());
+
+
+
         }
         
         
