af090275a56c2da5e95597644aa293a7143ebd13
Ertan Turan <ertanturan@outlook.com>
2021-06-13 17:54:12 +0300

    fixed some testing methods

--- GeodesyLib_UnitTest/CalculationTests.cs
+++ GeodesyLib_UnitTest/CalculationTests.cs
@@ -1,4 +1,3 @@
-
 using System;
 using GeodesyLib;
 using GeodesyLib.DataTypes;
@@ -62,16 +61,16 @@ namespace GeodesyLib_UnitTest
 
             //assert
 
-            Assert.That(result.Lat, Is.EqualTo(expectedResult.Lat));
-            Assert.That(result.Lon, Is.EqualTo(expectedResult.Lon));
+            Assert.AreEqual(expectedResult.Lat, result.Lat, 0.000000001d);
+            Assert.AreEqual(expectedResult.Lon, result.Lon, 0.000000001d);
+
         }
 
-        
 
         [Test]
         [TestCase(0, 52.205, 0.119)]
         [TestCase(1, 48.857, 2.351)]
-        [TestCase(0.5, 50.5363, 1.2746)]
+        [TestCase(0.5, 50.53632687827433d, 1.2746141006782352d)]
         public void CalculateIntermediatePoint_WhenCalled_ReturnsExactResult(double fraction,
             double expectedLat, double expectedLon)
         {
@@ -79,12 +78,12 @@ namespace GeodesyLib_UnitTest
             double distance = _from.HaversineDistance(_to);
 
             Coordinate result = _from.CalculateIntermediatePointByFraction(_to,
-                 fraction);
+                fraction);
 
 
             //assert
-            Assert.AreEqual(expectedLat, result.Lat, 0.0001d);
-            Assert.AreEqual(expectedLon, result.Lon, 0.0001d);
+            Assert.AreEqual(expectedLat, result.Lat, 0.0000001d);
+            Assert.AreEqual(expectedLon, result.Lon, 0.0000001d);
         }
 
         [Test]
@@ -108,12 +107,12 @@ namespace GeodesyLib_UnitTest
         {
             //act
 
-            Coordinate[] result = _from.Get_N_AmountOfCoordinatesBetween(_to,50);
+            Coordinate[] result = _from.Get_N_AmountOfCoordinatesBetween(_to, 50);
 
-         
-            
-            Assert.That(result.Length,Is.EqualTo(50));
+
+            Assert.That(result.Length, Is.EqualTo(50));
+            Assert.That(_from.Lat, Is.EqualTo(result[0].Lat));
+            Assert.That(_from.Lon, Is.EqualTo(result[0].Lon));
         }
-        
     }
 }
