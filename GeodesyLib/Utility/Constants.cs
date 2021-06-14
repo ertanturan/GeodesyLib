256a8ef3578f01b7a56bc25f94bed4da8a123563
Ertan Turan <ertanturan@outlook.com>
2021-06-14 20:47:42 +0300

    increased precision of pi

--- GeodesyLib/Utility/Constants.cs
+++ GeodesyLib/Utility/Constants.cs
@@ -3,8 +3,15 @@
     
     public static class Constants
     {
+        /// <summary>
+        /// Radius of the world in kilometers (KM)
+        /// </summary>
         public const double RADIUS = 6371;
-        public const double PI = 3.14159265359;
+        
+        /// <summary>
+        /// Pi number. Just didn't want to use the one given by the default Math library because it's more precise this way.
+        /// </summary>
+        public const double PI = 3.141592653589793238462643383279d;
     }
     
 }
